d3bbb4c88f941ce7c9092e4e4968bbf326d65da7
Vaibhav Pant <vpant4048@gmail.com>
2026-06-12 15:45:13 +0530

    feat: update js logic to handle window theme properly

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // window.matchMedia("(prefers-color-scheme: dark)").matches// if this is true means system is true -dark,false-light
 
 
-if(window.matchMedia("(prefers-color-scheme: dark)").matches){
+function setDarkLight() 
+    
+{if(window.matchMedia("(prefers-color-scheme: dark)").matches){
     document.body.classList.add("dark");
+    document.body.classList.remove("light");
 }
 else{
     document.body.classList.add("light");
-}
+    document.body.classList.remove("dark");
+}}
+setDarkLight();
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function () {
+    setDarkLight();
+})
