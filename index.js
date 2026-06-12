2069764deb4a86f3785ce2aed0a60bb408f5db70
Vaibhav Pant <vpant4048@gmail.com>
2026-06-12 15:51:33 +0530

    feat: added toggle theme logic in js file

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ else{
 }}
 setDarkLight();
 
+let btn=document.querySelector("button");
+
+btn.addEventListener("click", function(){
+    if(document.body.classList.contains("light")){
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");    
+    }
+    else{
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");             
+    }
+});
+
+
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function () {
     setDarkLight();
-})
+});
